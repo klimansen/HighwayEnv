adbe9118bcf68be6ce92a8aec179e561de310649
klimansen <146094905+klimansen@users.noreply.github.com>
2023-12-18 13:54:47 +0100

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -40,9 +40,10 @@ class HighwayEnv(AbstractEnv):
                 "collision_reward": -1,  # The reward received when colliding with a vehicle.
                 "right_lane_reward": 0.1,  # The reward received when driving on the right-most lanes, linearly mapped to
                 # zero for other lanes.
-                "high_speed_reward": 0.4,  # The reward received when driving at full speed, linearly mapped to zero for
+                # edited high speed reward from 0.4
+                "high_speed_reward": 0.6,  # The reward received when driving at full speed, linearly mapped to zero for
                 # lower speeds according to config["reward_speed_range"].
-                "lane_change_reward": 0.2,  # The reward received at each lane change action.
+                "lane_change_reward": -1,  # The reward received at each lane change action.
                 "reward_speed_range": [20, 30],
                 "normalize_reward": True,
                 "offroad_terminal": False,
